d1531b2a0e94b07a107497175beba83b417c3a61
Eric Kidd <git@randomhacks.net>
2010-05-13 12:12:54 -0400

    Copy unit test infrastructure from mongoid_attachment and adapt

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.setup(:default, :development)
+
+require 'spec'
+require 'spec/autorun'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(File.dirname(__FILE__)), 'lib'))
+
+require 'mongoid_references'
+
+def fixture_path(path)
+  File.join(File.dirname(__FILE__), 'fixtures', path)
+end
+
+Mongoid.configure do |config|
+  config.master = Mongo::Connection.new.db('mongoid_references_test')
+end
+
+Spec::Runner.configure do |config|  
+end
